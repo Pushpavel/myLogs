b0914686625e20c02ad9b1d50ded7a0c2984976c
CS19B1042 Pushpavel <jpushpavel@gmail.com>
2021-11-10 10:33:27 +0530

    🚧 add basic EditableLog.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,37 @@
 <script lang="ts">
-	export let name: string;
+    import EditableLog from "./components/EditableLog.svelte";
+    import Log from "./components/Log.svelte";
+
+    let logs = []
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <EditableLog/>
+    <!--    Filter-->
+    {#each logs as log}
+        <Log/>
+    {/each}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
---
+++ src/components/EditableLog.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {insertLog} from "../lib/insertLog";
+
+    let value = ""
+
+    function onSubmit(e) {
+        if (value && e.keyCode === 13) {
+            e.preventDefault()
+            insertLog({text: value, timestamp: Date.now()})
+            value = ""
+        }
+    }
+</script>
+<textarea bind:value on:keydown={onSubmit}></textarea>
