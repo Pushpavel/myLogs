d101fb13e75a9d1caeacd1a96d60e1c7ce1d696b
Vignesh <vickydhoni11@gmail.com>
2021-11-11 15:03:49 +0530

    Log CSS style done

--- src/components/Log.svelte
+++ src/components/Log.svelte
@@ -4,6 +4,6 @@
 
     export let log: Log;
 </script>
-<div>
+<div class=" py-2 px-4 border-gray-300 border-solid  rounded-lg w-1/2 m-1 shadow-md" style="border-width: 1.5px;">
     <SvelteMarkdown source={log.text}/>
 </div>
