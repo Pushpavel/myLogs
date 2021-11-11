f912a42608391b174efbf03f382e29c9d9b3ee7d
Subramanian <subseg1107@gmail.com>
2021-11-11 19:38:40 +0530

    Calender-Litepicker

--- src/App.svelte
+++ src/App.svelte
@@ -2,33 +2,46 @@
 <script lang="ts">
     import EditableLog from "./components/EditableLog.svelte";
     import Log from "./components/Log.svelte";
-    import {getLogs} from "./lib/api_interface";
-    import {onMount} from "svelte";
+    import { getLogs } from "./lib/api_interface";
+    import { onMount } from "svelte";
     import SearchBar from "./components/SearchBar.svelte";
+    import Litepicker from "litepicker";
 
-    let refreshPromise: Promise<Log[]> = null
-    let search = ""
-
+    let refreshPromise: Promise<Log[]> = null;
+    let search = "";
+    let litepicker = null;
     // retrieves logs from the server and updates the logs array
     async function refreshLogs() {
         refreshPromise = getLogs({
-            text: search
-        })
+            text: search,
+        });
     }
 
-    $:search, refreshLogs()
+    $: search, refreshLogs();
 
     // initial refresh
-    onMount(refreshLogs)
+    onMount(() => {
+        refreshLogs();
+        const picker = new Litepicker({
+            element: litepicker,
+            setup: (picker) => {
+                picker.on("button:apply", (date1, date2) => {
+                    
+                });
+            },
+        });
+    });
 </script>
 
-<EditableLog on:apply={refreshLogs}/>
-<SearchBar bind:search/>
+<button bind:this={litepicker}>Calender</button>
+
+<EditableLog on:apply={refreshLogs} />
+<SearchBar bind:search />
 {#await refreshPromise}
     Loading...
 {:then logs}
     {#each logs ?? [] as log}
-        <Log log={log}/>
+        <Log {log} />
     {/each}
 {/await}
 
