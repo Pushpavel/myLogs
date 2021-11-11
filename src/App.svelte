f3c7f2ff52c764d737e5da8c4bdc2b223ae81a68
CS19B1042 Pushpavel <jpushpavel@gmail.com>
2021-11-11 07:42:37 +0530

    🔥 remove unused styles

--- src/App.svelte
+++ src/App.svelte
@@ -22,36 +22,12 @@
     onMount(refreshLogs)
 </script>
 
-<main>
-    <EditableLog on:apply={refreshLogs}/>
-    <SearchBar bind:search/>
-    {#await refreshPromise}
-        Loading...
-    {:then logs}
-        {#each logs ?? [] as log}
-            <Log log={log}/>
-        {/each}
-    {/await}
-</main>
-
-<style>
-    main {
-        text-align: center;
-        padding: 1em;
-        max-width: 240px;
-        margin: 0 auto;
-    }
-
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
-    }
-
-    @media (min-width: 640px) {
-        main {
-            max-width: none;
-        }
-    }
-</style>
+<EditableLog on:apply={refreshLogs}/>
+<SearchBar bind:search/>
+{#await refreshPromise}
+    Loading...
+{:then logs}
+    {#each logs ?? [] as log}
+        <Log log={log}/>
+    {/each}
+{/await}
